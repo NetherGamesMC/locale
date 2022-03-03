bd55ecf9ef399893ac12b4f214922ac714254db1
seetch <seetch@inbox.ru>
2022-03-03 06:26:59 +0300

    Completely corrects the Russian translations

--- Russian.php
+++ Russian.php
@@ -26,25 +26,25 @@ class Russian implements Language
     public function translations(): array
     {
         return [
-            'command.chat.change' => '§6Disabled {%0} чат и включен {%1} чат . Вы сейчас общаться только с {%1} участниками.',
-            'command.chat.disable' => '§6Disabled {%0} chat. Теперь вы общаетесь со всеми игроками. ',
-            'command.chat.enable' => '§6Enabled {%0} chat. Теперь вы общаетесь только с {%0} участниками. ',
-            'command.chat.public' => '§c Извините, вы уже в публичном чате.',
-            'command.chat.usage' => '§cRun /c [{%0}] для включения определенного чата.',
-            'command.chat.usage.nothing' => '§c Извините, вы не можете переключать чаты, пока вы не в партии.',
-            'command.chat.usage.party' => '§cWoohoo! Запустите /c party, чтобы включить чат для вечеринки, запустите /c off, чтобы отключить чат для вечеринки. ',
-            'database.error' => '§l§cНеожиданная ошибка случилась вовремя подключения. Ваша дата не сохранилась.',
+            'command.chat.change' => '§6Отключен {%0} чат и включен {%1} чат. Вы сейчас общаться только с {%1} участниками.',
+            'command.chat.disable' => '§6Отключен {%0} чат. Теперь вы общаетесь со всеми игроками.',
+            'command.chat.enable' => '§6Включен {%0} чат. Теперь вы общаетесь только с {%0} участниками.',
+            'command.chat.public' => '§cИзвините, вы уже в публичном чате.',
+            'command.chat.usage' => '§cИспользуйте /c [{%0}] для включения определенного чата.',
+            'command.chat.usage.nothing' => '§cИзвините, вы не можете переключать чаты, пока вы не в группе.',
+            'command.chat.usage.party' => '§cУх! Используйте /c party, чтобы включить чат для группы, или /c off, чтобы отключить чат для группы. ',
+            'database.error' => '§l§cНеожиданная ошибка случилась вовремя подключения. Ваши данные не сохранились.',
             'forms.profile.credits' => '§bКредиты: {%0}',
-            'forms.profile.deaths' => '§bСмерти : {%0}',
-            'forms.profile.kills' => '§bУбивства: {%0}',
+            'forms.profile.deaths' => '§bСмерти: {%0}',
+            'forms.profile.kills' => '§bУбийства: {%0}',
             'forms.profile.level' => '§bУровень: {%0}',
             'forms.profile.stats' => '§aВаша статистика:',
             'forms.profile.xp' => '§bXP: {%0}',
-            'join.vote' => '§6Голосуйте за нас ежедневно по §bngmc.co/v §6и вы сможете выиграть бесплатное ранг обновить!',
+            'join.vote' => '§6Голосуйте за нас ежедневно на §bngmc.co/v §6и вы сможете бесплатно обновить ранг!',
             'join.welcome' => '§bДобро пожаловать в §Nether§6Games§b!',
-            'minigame.party.member.join' => '§cВы сейчас на вечеринке. Подождите, пока хозяин вашей партии не решит, в какую игру играть!',
-            'skin.invalid.change' => '§c Ваш скин не подходит для воспроизведения на §Nether§6Games§c. Пожалуйста, выберите другой.',
-            'social.party.notin' => '§cВы не на вечеринке!',
+            'minigame.party.member.join' => '§cВы сейчас в группе. Подождите, пока хозяин вашей группы не решит, в какую игру играть!',
+            'skin.invalid.change' => '§cВаш скин не подходит для использования на §Nether§6Games§c. Пожалуйста, выберите другой.',
+            'social.party.notin' => '§cВы не состоите в группе!',
         ];
     }
 
@@ -65,6 +65,6 @@ class Russian implements Language
 
     public function getCredits(): string
     {
-        return 'SashieCat & xSebyMCPEYT';
+        return 'SashieCat & xSebyMCPEYT & seetch';
     }
 }
