2f3c66492071437f7dbfba7763206e06947d2267
Aideon <15018948+PiggyPlex@users.noreply.github.com>
2022-04-07 04:57:59 +0100

    Update French.php

--- French.php
+++ French.php
@@ -32,7 +32,9 @@ class French implements Language
             'command.chat.public' => '§cDésolé, vous êtes déjà dans le chat public.',
             'command.chat.usage' => '§cEntrez /c [{%0}] pour activer un tchat spécifique.',
             'command.chat.usage.nothing' => "§cDésolé, vous ne pouvez pas changer de tchat quand vous n'êtes pas dans un groupe.",
-            'command.chat.usage.party' => '§cWoohoo! Entrez /c party pour activer le tchat de groupe, entrez /c off pour le désactiver.',
+            'command.chat.usage.party' => '§cWoohoo ! Entrez /c party pour activer le tchat de groupe, entrez /c off pour le désactiver.',
+            'command.reserved.estaff' => '§cCette commande est réservée au personnel exécutif de §eNether§6Games§c.',
+            'command.reserved.staff' => '§cCette commande est réservée au personnel de §eNether§6Games.§c.',
             'database.error' => '§l§cUne erreur inattendue est survenue lors de la connexion à la base de données. Vos données pour cette session ne vont pas être sauvegardés.',
             'forms.profile.credits' => '§bCrédits: {%0}',
             'forms.profile.deaths' => '§bMorts: {%0}',
@@ -44,7 +46,11 @@ class French implements Language
             'join.welcome' => '§bBienvenue sur §eNether§6Games §b!',
             'minigame.party.member.join' => '§cVous êtes actuellement dans un groupe. Attendez que le chef décide à quel jeu vous allez jouer !',
             'skin.invalid.change' => '§cVotre skin n\'est pas valide pour jouer sur §eNether§6Games§c. Choisissez-en un autre.',
-            'social.party.notin' => "§cVous n'êtes pas dans un groupe!",
+            'social.party.notin' => "§cVous n'êtes pas dans un groupe !",
+            'kick.maintenance' => '§o§l§eN§6G§r§7: §cMaintenance en cours. Suivez-nous sur Twitter pour les mises à jour - §6@NetherGamesMC§c.',
+            'kick.client.outdated' => '§o§l§eN§6G§r§7: §cMettez à jour votre client à la dernière version pour jouer à §eNether§6Games§c.',
+            'kick.server.outdated' => "§o§l§eN§6G§r§7: §eNether§6Games §cn'a pas encore été mis à jour à la dernière version. Suivez-nous sur Twitter pour les mises à jour - §6@NetherGamesMC§c.",
+            'minigame.party.member.join' => "§cVous êtes actuellement dans un groupe. Attendez que votre hôte décide du jeu auquel vous allez jouer !"
         ];
     }
 
